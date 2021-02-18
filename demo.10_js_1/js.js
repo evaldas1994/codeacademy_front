2f909f07210af17b3e3653e33a36c217325ee367
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-02-18 21:59:42 +0200

    Get element by id [ ]
    .textContent
    .style.background

--- demo.10_js_1/js.js
+++ demo.10_js_1/js.js
@@ -2,3 +2,4 @@ document.getElementsByTagName("li")[0].textContent = "Labas";
 document.getElementsByTagName("li")[1].textContent = "Labas";
 document.getElementsByTagName("li")[2].textContent = "Labas";
 document.getElementsByTagName("h1")[0].textContent = "Bye";
+document.getElementsByTagName("h1")[0].style.background = "red";
