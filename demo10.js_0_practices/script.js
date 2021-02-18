993710432bf511eaeeaf8dd5fa17cd052e8a56d8
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-02-18 22:10:01 +0200

    Practices of basic data types

---
+++ demo10.js_0_practices/script.js
@@ -0,0 +1,40 @@
+console.log(8 + "16" + 1);
+console.log(8 + 8 + "Petras" + 5 + 5);
+console.log(8 >= 8);
+console.log(10 === "10");
+console.log(5 == "5");
+console.log(12 !== "12");
+console.log((7 > 1) && (8 < 5));
+console.log(10 * 10 == 100);
+console.log(10 / 10 === "1");
+console.log(15 > 10 || 15 < 5);
+console.log(true && false);
+console.log(true || false);
+console.log((true && true) || false);
+console.log("Microsoft" === "microsoft");
+console.log("Google" == "gogole");
+console.log(10 + "15" + 10 === "10" + 15 + 10);
+console.log(10 && true);
+console.log(10 % 1 === 10);
+console.log(10 / 1 !== 10);
+console.log(5 * "5" === 25);
+
+const name = "Evaldas";
+if((name.length * 5) >= 30) {
+    //alert("Ilgas vardas");
+}else {
+    alert("Trumpas vardas");
+}
+
+const age = 20;
+let myAge = 26;
+
+if(myAge > 150 || myAge < 0) {
+    console.log('Įvesti normalų amžių')
+} else  if(myAge >= age) {
+    console.log("Galima gerti vyną");
+}else {
+    console.log("Negalima gerti vyno");
+}
+
+
