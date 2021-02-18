a64027f174fd59fa9d7fbbd6430039c82015cae0
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-02-18 21:46:40 +0200

    Get element by id [ ]

---
+++ demo.10_js_1/js.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName("li")[0].textContent = "Labas";
+document.getElementsByTagName("li")[1].textContent = "Labas";
+document.getElementsByTagName("li")[2].textContent = "Labas";
